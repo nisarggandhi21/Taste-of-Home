a16b9403a2d5fe54778083e47de0ce8398866e8f
Nisarg Gandhi <nisarggandhi21@gmail.com>
2024-03-08 18:56:02 +0530

    update getting token from local storage

--- api/middleware/jwt.js
+++ api/middleware/jwt.js
@@ -2,9 +2,8 @@ import jwt from "jsonwebtoken";
 import createError from "../utils/createError.js";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.accessToken;
+  const token = localStorage.getItem("currentUser"); // Retrieve token from localStorage
 
-  if (!token) console.log("no token");
   if (!token) return next(createError(401, "You are not authenticated!"));
 
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
