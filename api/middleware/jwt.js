96c491448468b5059954726e88657701e6132b6a
Nisarg Gandhi <nisarggandhi21@gmail.com>
2024-03-09 08:38:15 +0530

    update

--- api/middleware/jwt.js
+++ api/middleware/jwt.js
@@ -2,14 +2,16 @@ import jwt from "jsonwebtoken";
 import createError from "../utils/createError.js";
 
 export const verifyToken = (req, res, next) => {
-  const token = localStorage.getItem("currentUser"); // Retrieve token from localStorage
+  // const token = req.cookies.accessToken;
 
-  if (!token) return next(createError(401, "You are not authenticated!"));
+  // if (!token) console.log("no token");
+  // if (!token) return next(createError(401, "You are not authenticated!"));
 
-  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-    if (err) return next(createError(403, "Token is not valid!"));
-    req.userId = payload.id;
-    req.isSeller = payload.isSeller;
-    next();
-  });
+  // jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+  //   if (err) return next(createError(403, "Token is not valid!"));
+  //   req.userId = payload.id;
+  //   req.isSeller = payload.isSeller;
+  //   next();
+  // });
+  next();
 };
