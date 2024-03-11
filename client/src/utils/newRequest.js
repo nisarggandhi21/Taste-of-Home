6618950e2e28fea34ed2a6caaa13598e7150f627
Nisarg Gandhi <nisarggandhi21@gmail.com>
2024-03-11 12:30:45 +0530

    Working Properly

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://taste-of-home-aim1.onrender.com/api/",
+  baseURL: "http://localhost:8000/api/",
   withCredentials: true,
 });
 
