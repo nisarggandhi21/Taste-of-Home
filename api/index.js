f546b58c5ce76808c91a2ec6203387c055cb5dd0
Nisarg Gandhi <nisarggandhi21@gmail.com>
2024-11-13 01:47:48 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+
+mongoose.set("strictQuery", true);
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to database");
+  } catch (error) {
+    console.log("Error connecting to database: ", error);
+  }
+};
 
 // Middleware
 app.use(cors());
@@ -15,5 +28,6 @@ app.get("/", (req, res) => {
 // Set port and start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+  connect();
   console.log(`Server is running on port ${PORT}`);
 });
