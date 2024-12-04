ebb35becca0dcb7483edcfa3e83d0c181746a635
Nisarg Gandhi <nisarggandhi21@gmail.com>
2024-12-05 01:28:27 +0530

    Updated frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
-import "./app.scss";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import React from "react";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import Home from "./pages/home/Home";
 import Items from "./pages/items/Items";
 import Item from "./pages/item/Item";
@@ -15,81 +14,42 @@ import Message from "./pages/message/Message";
 import MyItems from "./pages/myItems/MyItems";
 import Pay from "./pages/pay/Pay";
 import Success from "./pages/success/Success";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+
+import "./app.scss";
 
 const queryClient = new QueryClient();
 
-function App() {
-  const Layout = () => {
-    return (
-      <div className="app">
-        <QueryClientProvider client={queryClient}>
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <div className="app">
           <Navbar />
-          <Outlet />
+          <Routes>
+            {/* Main routes within layout */}
+            <Route path="/" element={<Home />} />
+            <Route path="/items" element={<Items />} />
+            <Route path="/myitems" element={<MyItems />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/message/:id" element={<Message />} />
+            <Route path="/add" element={<Add />} />
+            <Route path="/item/:id" element={<Item />} />
+            <Route path="/pay/:id" element={<Pay />} />
+            <Route path="/success" element={<Success />} />
+
+            {/* Authentication routes */}
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
           <Footer />
-        </QueryClientProvider>
-      </div>
-    );
-  };
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/items",
-          element: <Items />,
-        },
-        {
-          path: "/myitems",
-          element: <MyItems />,
-        },
-        {
-          path: "/orders",
-          element: <Orders />,
-        },
-        {
-          path: "/messages",
-          element: <Messages />,
-        },
-        {
-          path: "/message/:id",
-          element: <Message />,
-        },
-        {
-          path: "/add",
-          element: <Add />,
-        },
-        {
-          path: "/item/:id",
-          element: <Item />,
-        },
-        {
-          path: "/pay/:id",
-          element: <Pay />,
-        },
-        {
-          path: "/success",
-          element: <Success />,
-        },
-      ],
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
-}
+        </div>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
